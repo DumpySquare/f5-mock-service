d74597e5f20638b3ff6d07d72cd50bc9d22bc309
DumpySquare <bunookana@gmail.com>
2022-02-01 13:54:30 +0000

    remove dec validation work

---
+++ tests/some.tests.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+
+
+describe('yay tests', function () {
+
+
+
+    it('some test', async function () {
+        assert.ok(1)
+    });
+
+
+});
